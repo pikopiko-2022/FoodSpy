7d6c6d85c6973f00bef96ccc6c79b6f38f67cce8
Angela Kitt <angelakitt@Angelas-Air.hub>
2022-09-21 16:55:34 +1200

    created files

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,23 +1,10 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { fetchFruits } from '../actions'
+import React from 'react'
 
 function Home() {
-  const fruits = useSelector((state) => state.fruits)
-  const dispatch = useDispatch()
-  useEffect(() => {
-    dispatch(fetchFruits())
-  }, [])
-
   return (
     <>
       <div>
         <h1>FoodSpy</h1>
-        <ul>
-          {fruits.map((fruit) => (
-            <li key={fruit}>{fruit}</li>
-          ))}
-        </ul>
       </div>
     </>
   )
