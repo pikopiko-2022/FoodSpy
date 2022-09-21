edfe8cf872fe371fca04629eecdc52788a65ea7c
Jayne Hamilton <jayne@Jaynes-MacBook-Pro.local>
2022-09-21 16:49:24 +1200

    nav initial set up

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faUserSecret,
+  faBinoculars,
+  faMagnifyingGlassDollar,
+  faCartShopping,
+} from '@fortawesome/free-solid-svg-icons'
 
 function Nav() {
-  //CODE HERE
+  return (
+    <nav className="nav">
+      <h1 className="logo">
+        <Link to={'/'}> FoodSpy</Link>
+        <FontAwesomeIcon icon={faUserSecret} />
+        <FontAwesomeIcon icon={faBinoculars} />
+        <FontAwesomeIcon icon={faMagnifyingGlassDollar} />
+        <FontAwesomeIcon icon={faCartShopping} />
+      </h1>
+      {/* click button and direct to the update form to complete */}
+      <div className="nav-update-btn">
+        <button>UPDATE ITEM</button>
+      </div>
+    </nav>
+  )
 }
 
 export default Nav
