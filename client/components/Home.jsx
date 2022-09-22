1b0670bcd45c86a557d12e1201d162226f191872
shantaliah-filipo <107346170+shantaliah-filipo@users.noreply.github.com>
2022-09-22 20:48:42 +1200

    Remove dropdown for items

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,82 +1,47 @@
 import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import { getItem } from '../apis/apiClient'
-
+import { useDispatch, useSelector, Link } from 'react-router-dom'
+import Dropdown from './Dropdown'
 import styles from './Home.module.scss'
-import Item from './Item'
 
+import { fetchHomeContent } from '../actions/home'
+// how the bloody hell do you get an image from server-side rendered?
+// let image = require('../../server/public/images/Piggy.jpg')
 function Home() {
-  const [item, setItem] = useState('')
+  const dispatch = useDispatch()
+  const homeContent = useSelector((state) => state.home)
 
-  let { id } = useParams()
+  // useEffect(() => dispatch(fetchHomeContent), [])
 
-  useEffect(() => {
-    getItem(id)
-      .then(() => {
-        setItem()
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  }, [])
-  
-  
+  return (
+    <div>
+      <div className={styles.Home}>
+        <img src={homeContent.imageUrl} alt="homePageImage"></img>
 
-
-    // const [city, setCity] = useState()
-    // useState
-
-    // function to handleClick
-    // function to handle change?
-    // const handleClick(e) {
-    //   e.target.value
-    // }
-
-    // const handleSubmit(e) {
-    //   e.preventDefault()
-    // }
-    // map over the items
-
-    return (
-      <>
-        {/* Drop down inserted to this page */}
         <div>
           <h1>A place to find the bare essentials on a budget </h1>
         </div>
 
-        <div className={styles.Home}>
-          <form className={styles.dropdown}>
-            <b> Where are you? </b>
-            <select id="myList" onChange="favTutorial()">
-              <option> ---Choose city--- </option>
-              <option> Auckland </option>
-              <option> Napier </option>
-              <option> Wellington </option>
-              <option> Christchurch</option>
-            </select>
-          </form>
-        </div>
+        <form className={styles.dropdown}>
+          <b> Where are you? </b>
+          <select id="myList" onChange="favTutorial()">
+            <option> ---Choose city--- </option>
+            <option> Auckland </option>
+            <option> Napier </option>
+            <option> Wellington </option>
+            <option> Christchurch</option>
+          </select>
+        </form>
+      </div>
 
-        <div>
-          <form>
-            <b> What are you looking for? </b>
-            <select id="myList" onChange="foodItem()">
-              <option> ---Select--- </option>
-              <option> Milk</option>
-              <option> Eggs </option>
-              <option> Bread</option>
-              <option>Shopping List</option>
-            </select>
-          </form>
-        </div>
-        <div>
-          <Link to="./Item">
-            <button className="button">Show me the bargains!</button>
-          </Link>
-        </div>
-      </>
-    )
-  }
+      <div className={styles.dropdown}>
+        <Dropdown placeHolder="Select..." />
+      </div>
+
+      <Link to="./Item">
+        <button className="button">Show me the bargains!</button>
+      </Link>
+    </div>
+  )
 }
 
 export default Home
