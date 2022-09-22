f5242d15992087c1a4a0abe14926e40ac00ac1a9
Kirsty Brewin <107161696+kirsty-brewin@users.noreply.github.com>
2022-09-22 17:50:29 +1200

    Add Home component and scss

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,14 +1,82 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { getItem } from '../apis/apiClient'
+
+import styles from './Home.module.scss'
+import Item from './Item'
 
 function Home() {
-  return (
-    <>
-      {/* Drop down inserted to this page */}
-      <div>
-        <h1>A platform to find the best prices on essentials </h1>
-      </div>
-    </>
-  )
+  const [item, setItem] = useState('')
+
+  let { id } = useParams()
+
+  useEffect(() => {
+    getItem(id)
+      .then(() => {
+        setItem()
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }, [])
+  
+  
+
+
+    // const [city, setCity] = useState()
+    // useState
+
+    // function to handleClick
+    // function to handle change?
+    // const handleClick(e) {
+    //   e.target.value
+    // }
+
+    // const handleSubmit(e) {
+    //   e.preventDefault()
+    // }
+    // map over the items
+
+    return (
+      <>
+        {/* Drop down inserted to this page */}
+        <div>
+          <h1>A place to find the bare essentials on a budget </h1>
+        </div>
+
+        <div className={styles.Home}>
+          <form className={styles.dropdown}>
+            <b> Where are you? </b>
+            <select id="myList" onChange="favTutorial()">
+              <option> ---Choose city--- </option>
+              <option> Auckland </option>
+              <option> Napier </option>
+              <option> Wellington </option>
+              <option> Christchurch</option>
+            </select>
+          </form>
+        </div>
+
+        <div>
+          <form>
+            <b> What are you looking for? </b>
+            <select id="myList" onChange="foodItem()">
+              <option> ---Select--- </option>
+              <option> Milk</option>
+              <option> Eggs </option>
+              <option> Bread</option>
+              <option>Shopping List</option>
+            </select>
+          </form>
+        </div>
+        <div>
+          <Link to="./Item">
+            <button className="button">Show me the bargains!</button>
+          </Link>
+        </div>
+      </>
+    )
+  }
 }
 
 export default Home
