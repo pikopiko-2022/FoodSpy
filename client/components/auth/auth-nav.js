d3d399fdbf2ab8d44565b4848b622cf192b9040f
Jayne Hamilton <jayne@Jaynes-MacBook-Pro.local>
2022-09-23 14:39:58 +1200

    Login button added to nav

---
+++ client/components/auth/auth-nav.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import AuthenticationButton from './authentication-button'
+
+const AuthNav = () => (
+  <div className="login-btn">
+    <AuthenticationButton />
+  </div>
+)
+
+export default AuthNav
