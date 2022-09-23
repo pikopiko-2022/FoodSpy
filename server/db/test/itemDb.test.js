221b6c11eef61c93b5c960da34204ee9fba2d8b8
shantaliah-filipo <107346170+shantaliah-filipo@users.noreply.github.com>
2022-09-23 17:26:31 +1200

    Test if the itemDb function returns data from table

--- server/db/test/itemDb.test.js
+++ server/db/test/itemDb.test.js
@@ -12,12 +12,42 @@ beforeEach(() => {
   return testDb.seed.run()
 })
 
-describe('getItem', () => {
+describe('getItems', () => {
   it('returns the item array', () => {
+    expect.assertions(3)
     return db.getItems(testDb).then((item) => {
       expect(item).toHaveLength(3)
-      expect(item[0]).toHaveProperty('name')
-      expect(item[0].name).toBe('Milk')
+      expect(item[0]).toHaveProperty('item_name')
+      expect(item[0].item_name).toBe('Milk')
+    })
+  })
+})
+
+describe('get single Item', () => {
+  it('returns a single item', () => {
+    expect.assertions(2)
+    return db.getItem(1, testDb).then((item) => {
+      expect(item).toHaveProperty('description')
+      expect(item.description).toBe('2L lite blue milk')
+    })
+  })
+})
+
+
+//ask for help to see if update is actually getting tested
+describe('update Item', () => {
+  it('returns updated item', () => {
+    const fakeData = {
+      price: 3
+    }
+    return db.updatePrice(1, fakeData.price, testDb).then((item) => {
+      console.log(item)
+      return testDb('itemPrices').select()
+    })
+    .then(itemPrices => {
+      console.log(itemPrices[1])
+      expect(itemPrices[1].item_id).toBe(1)
+      expect(itemPrices[1].price).toBe(3.84)
     })
   })
 })
