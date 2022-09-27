23ad41f644b7d1711a602ceee731d7d8f720d836
shantaliah-filipo <107346170+shantaliah-filipo@users.noreply.github.com>
2022-09-28 08:58:47 +1300

    Remove duplicate heading

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -100,7 +100,7 @@ export default function Home() {
         <div>
           <h1>OR</h1>
         </div>
-        <h1>OR</h1>
+
         <Link className="createList" to="/list">
           Create shopping list
         </Link>
